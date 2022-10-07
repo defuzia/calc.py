0ae5c8aefcad58ad0b81ae20e8b2ccec4bcacfaf
defuzia <zhyvchyn.roman@gmail.com>
2022-10-07 14:04:27 +0300

    шось

--- calc.py
+++ calc.py
@@ -61,46 +61,114 @@
 #
 
 
-
-
-
 ###############################################################################################################################################
 
 
+# while True:
+#         message = input()
+#         word = []
+#
+#         alphabets = ["abcdefghijklmnopqrstuvwxyz", "абвгґдеєжзиіїйклмнопрстуфхцчшщьюя", "0123456789"]
+#         for i in message:
+#                 if i in alphabets[0]:
+#                         if i == alphabets[0][-1]:
+#                                 index = 0
+#                         else:
+#                                 index = alphabets[0].index(i) + 1
+#                         word.append(alphabets[0][index])
+#                 elif i in alphabets[1]:
+#                         if i == alphabets[1][-1]:
+#                                 index = 0
+#                         else:
+#                                 index = alphabets[1].index(i) + 1
+#                         word.append(alphabets[1][index])
+#                 elif i in alphabets[2]:
+#                         if i == alphabets[2][-1]:
+#                                 index = 0
+#                         else:
+#                                 index = alphabets[2].index(i) + 1
+#                         word.append(alphabets[2][index])
+#                 else:
+#                         word.append(i)
+#
+#
+#
+#         res = ''
+#         for i in word:
+#                 res += i
+#         print(res)
 
 
+# while True:
+#     alphabet = {"eng": "abcdefghijklmnopqrstuvwxyz",
+#                 "ukr": "абвгґдеєжзиіїйклмнопрстуфхцчшщьюя",
+#                 "nums": "0123456789"
+#                 }
+#     encrypt = input("Enter a clear message: ").lower()
+#     encrypted = ""
+#     for letter in encrypt:
+#         if letter in alphabet["eng"]:
+#             position = alphabet["eng"].find(letter) + 1
+#             if letter in alphabet["eng"][-1]:
+#                 encrypted = encrypted + alphabet["eng"][0]
+#             else:
+#                 encrypted = encrypted + alphabet["eng"][position]
+#         elif letter in alphabet["ukr"]:
+#             position = alphabet["ukr"].find(letter) + 1
+#             if letter in alphabet["ukr"][-1]:
+#                 encrypted = encrypted + alphabet["ukr"][0]
+#             else:
+#                 encrypted = encrypted + alphabet["ukr"][position]
+#         elif letter in alphabet["nums"]:
+#             position = alphabet["nums"].find(letter) + 1
+#             if letter in alphabet["nums"][-1]:
+#                 encrypted = encrypted + alphabet["nums"][0]
+#             else:
+#                 encrypted = encrypted + alphabet["nums"][position]
+#         else:
+#             encrypted = encrypted + letter
+#     print(encrypted)
 
 
-while True:
-        message = input()
-        word = []
+text = input("Введіть текс: ")
+mode = input("Введіть режим редагування(A/B/C): ")
 
-        alphabets = ["abcdefghijklmnopqrstuvwxyz", "абвгґдеєжзиіїйклмнопрстуфхцчшщьюя", "0123456789"]
-        for i in message:
-                if i in alphabets[0]:
-                        if i == alphabets[0][-1]:
-                                index = 0
-                        else:
-                                index = alphabets[0].index(i) + 1
-                        word.append(alphabets[0][index])
-                elif i in alphabets[1]:
-                        if i == alphabets[1][-1]:
-                                index = 0
-                        else:
-                                index = alphabets[1].index(i) + 1
-                        word.append(alphabets[1][index])
-                elif i in alphabets[2]:
-                        if i == alphabets[2][-1]:
-                                index = 0
-                        else:
-                                index = alphabets[2].index(i) + 1
-                        word.append(alphabets[2][index])
-                else:
-                        word.append(i)
+edited_text = []
+output = ""
+for i in text.split():
+    i = i.rstrip(",")
+    i = i.rstrip(".")
+    i = i.lower()
+    if mode == "A" and len(i) > 3:
+        edited_text.append(i)
+        edited_text.sort()
+    elif mode == "B" and len(i) > 3:
+        edited_text.append(i)
+    elif mode == "C" and len(i) > 3:
+        edited_text.append(i)
 
+if edited_text and mode == "A":
+    edited_text = set(edited_text)
+    edited_text = list(edited_text)
+    edited_text.sort()
+    for i in edited_text:
+        output += i + "\n"
+    print(output)
+elif edited_text and mode == "B":
+    output = {}
+    for i in edited_text:
+        word_counter = edited_text.count(i)
+        output[i] = word_counter
 
+    sorted_dict = sorted(output.items(), key=lambda x: x[1])
+    for i, j in sorted_dict[-1:-6:-1]:
+        print(i, "-", j)
 
-        res = ''
-        for i in word:
-                res += i
-        print(res)
+elif edited_text and mode == "C":
+    edited_text.sort(key=len)
+    edited_text = edited_text[-1:-6:-1]
+
+    for i in edited_text:
+        output += i + "\n"
+
+    print(output)
